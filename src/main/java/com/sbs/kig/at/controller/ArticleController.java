60b7383d95ff1e7b561873909bd43731bac03e21
KimIlGu <rladlfrn22@gmail.com>
2020-08-12 09:32:45 +0900

    기존 방식과 Model JSTL 방식, 오류 코드 수정

--- src/main/java/com/sbs/kig/at/controller/ArticleController.java
+++ src/main/java/com/sbs/kig/at/controller/ArticleController.java
@@ -14,41 +14,39 @@ import com.sbs.kig.at.service.ArticleService;
 
 @Controller
 public class ArticleController {
-	
-	/* 
-		ArticleController() {
-			ArticleService articleService = new ArticleService()
-		}
-		
-		생성자를 통해 서비스를 만들어야 했는데, Autowired는 스스로가 알아서 찾아간다.
-		대신 Service에서 @Service를 써주어야 한다. 
-		보통은 @Component를 써주는데 서비스는 특별하다. 
-		
-		크게 신경쓰고 있지 않았는데 @Controller도 같은 경우로 보면 될듯 하다.
-	*/
+
+	/*
+	 * ArticleController() { ArticleService articleService = new ArticleService() }
+	 * 
+	 * 생성자를 통해 서비스를 만들어야 했는데, Autowired는 스스로가 알아서 찾아간다. 대신 Service에서 @Service를 써주어야
+	 * 한다. 보통은 @Component를 써주는데 서비스는 특별하다.
+	 * 
+	 * 크게 신경쓰고 있지 않았는데 @Controller도 같은 경우로 보면 될듯 하다.
+	 */
 	@Autowired
 	private ArticleService articleService;
-	
+
 	@RequestMapping("/article/list1")
 	public String showList1(HttpServletRequest req) {
-		int count = articleService.getCount(); 
-		
+		int count = articleService.getCount();
+		List<Article> articles = articleService.getForPrintArticles();
+
 		req.setAttribute("count", count);
+		req.setAttribute("articles", articles);
 
 		return "article/list";
 	}
-	
-	// Model이 더 진보된 방식이라는데 이건 
+
+	// Model이 더 진보된 방식이라는데 이건 아직은 잘 모르겠다
 	@RequestMapping("/article/list2")
 	public String showList2(Model model) {
 		int count = articleService.getCount();
 		List<Article> articles = articleService.getForPrintArticles();
-		
+
 		model.addAttribute("count", count);
 		model.addAttribute("articles", articles);
-		
+
 		return "article/list";
 	}
-	
-	
+
 }
--- src/main/webapp/WEB-INF/jsp/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/article/list.jsp
@@ -1,13 +1,16 @@
-<%@ page import="com.sbs.java.blog.util.Util"%>
-<%@ page import="com.sbs.java.blog.util.Util"%>
+<%@ page import="java.util.List"%>
+<%@ page import="com.sbs.kig.at.dto.Article"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
+
+<!-- 기존 방식 
 <%
-List<Article> articles = (List<Article>)request.getAttribute("count");
+List<Article> counts = (List<Article>)request.getAttribute("count");
 List<Article> articles = (List<Article>)request.getAttribute("articles");
 %>
+-->
 
 <!DOCTYPE html>
 <html>
@@ -20,11 +23,11 @@ List<Article> articles = (List<Article>)request.getAttribute("articles");
 	
 	<c:forEach items="${articles}" var="article">
 		<div>
-			${article.id}
-			${article.regDate}
-			${article.updateDate}
-			${article.title}
-			${article.body}
+			${article.id} <br />
+			${article.regDate} <br />
+			${article.updateDate} <br />
+			${article.title} <br />
+			${article.body} <br />
 		</div>
 	</c:forEach>	
 	
