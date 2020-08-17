257ecadd7a2978a5869b0bd8ed08061e8ecdb018
KimIlGu <rladlfrn22@gmail.com>
2020-08-17 23:12:53 +0900

    가입 완료시 완료 메일 발송

--- src/main/java/com/sbs/kig/at/controller/HomeController.java
+++ src/main/java/com/sbs/kig/at/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.sbs.kig.at.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	@Autowired
+	
 	// jsp를 사용하기 위해 pom.xml에서 JSP 엔진(jasper)을 추가한다.
 	@RequestMapping("/usr/home/main")
 	public String showMain() {
